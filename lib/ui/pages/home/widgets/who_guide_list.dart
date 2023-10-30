7886d29c867cec301a1d848c6967f57700bf144b
unjoo31 <unjoo31@gmail.com>
2023-10-30 20:59:25 +0900

    git commit "홈화면 > 누구나 가이드 > 상세보기 화면 연결 완료"

--- lib/ui/pages/home/widgets/who_guide_list.dart
+++ lib/ui/pages/home/widgets/who_guide_list.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 import '../../../../data/model/who_guide.dart';
 import '../../components/who_guide_list_item.dart';
+import '../../who_guide/widgets/who_guide_detail_form.dart';
 import 'home_top_button.dart';
 
 class WhoGuideList extends StatefulWidget {
@@ -44,6 +45,16 @@ class _WhoGuideListState extends State<WhoGuideList> {
                 userName: list[index].userName,
                 title: list[index].title,
                 picUrlFirst: list[index].picUrlFirst,
+                onDetailPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => WhoGuideDetailForm(
+                        selectedIndex: index, // 선택된 인덱스 전달
+                      ),
+                    ),
+                  );
+                },
               ),
               childCount: list.length,
             ),
