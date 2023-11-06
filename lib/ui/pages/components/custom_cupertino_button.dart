69d4f2102ba62c5de0c464bab2d3deceee46421a
unjoo31 <unjoo31@gmail.com>
2023-11-06 10:56:45 +0900

    git commit "마이페이지 > 로그아웃 완료"

---
+++ lib/ui/pages/components/custom_cupertino_button.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+import '../../../_core/constants/colors.dart';
+import '../../../_core/constants/move.dart';
+
+class CustomCupertinoButton extends StatelessWidget {
+  final String? text;
+  final GestureTapCallback? press;
+  final Color color;
+
+  const CustomCupertinoButton(
+      {Key? key, this.text, this.press, this.color = kPrimaryColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 10.0),
+      child: TextButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return Dialog(
+                backgroundColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Container(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        "로그아웃",
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 16.0),
+                      Text(
+                        "로그아웃 하시면 서비스 사용이 불가능하며 \n초기화면으로 돌아갑니다. \n정말로 로그아웃 하시겠습니까?",
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(height: 16.0),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: Container(
+                              width: 120,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(15.0),
+                                border: Border.all(
+                                    color: kPrimaryColor, width: 2.0),
+                              ),
+                              padding: EdgeInsets.all(10.0),
+                              child: Text(
+                                "취소",
+                                style: TextStyle(color: kPrimaryColor),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, Move.loginPage);
+                            },
+                            child: Container(
+                              width: 120,
+                              decoration: BoxDecoration(
+                                color: kPrimaryColor,
+                                borderRadius: BorderRadius.circular(15.0),
+                                border: Border.all(
+                                    color: kPrimaryColor, width: 2.0),
+                                // 테두리 색상 및 두께
+                              ),
+                              padding: EdgeInsets.all(10.0),
+                              child: Text(
+                                "로그아웃",
+                                style: TextStyle(color: Colors.white),
+                                textAlign: TextAlign.center,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+        style: TextButton.styleFrom(
+          backgroundColor: kPrimaryColor,
+          minimumSize: Size(400, 50),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
+        child: Text(
+          "${text}",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
--- lib/ui/pages/my/widgets/my_list_form.dart
+++ lib/ui/pages/my/widgets/my_list_form.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:project_all_guide_front/ui/pages/components/middle_text.dart';
+
+import '../../components/custom_cupertino_button.dart';
 
 class MyListForm extends StatelessWidget {
   const MyListForm({super.key});
@@ -6,8 +9,38 @@ class MyListForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("MyListForm"),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        toolbarHeight: 70,
+        centerTitle: false,
+        flexibleSpace: Container(
+          alignment: Alignment.topLeft,
+          padding: const EdgeInsets.only(top: 30.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.asset(
+                'assets/main_top_logo.png',
+                height: 60,
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            MiddleText(text: "마이페이지"),
+            CustomCupertinoButton(
+              text: "로그아웃",
+              press: () {
+                Navigator.pushNamed(context, 'guidemakesecondform');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
